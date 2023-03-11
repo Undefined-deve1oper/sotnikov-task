a2fced291a490d9bcd0c410b50cead67cfb6c57f
Undefined-deve1oper <kononovkirill05@gmail.com>
2023-03-11 13:41:40 +0600

    added posts routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const router = express.Router({ mergeParams: true });
 const userRouter = require("./user.routes");
 const authRouter = require("./auth.routes");
+const postsRouter = require("./post.routes");
 
 router.use("/users", userRouter);
 router.use("/auth", authRouter);
+router.use("/posts", postsRouter);
 
 module.exports = router;
