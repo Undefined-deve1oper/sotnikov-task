af6e0f85808a4395dd5372d318edf079daf19ec9
Undefined-deve1oper <kononovkirill05@gmail.com>
2023-03-11 11:40:49 +0600

    init project

---
+++ src/app/assets/index.js
@@ -0,0 +1,18 @@
+export { default as cakeIcon } from "./svg/cake.svg";
+export { default as cameraIcon } from "./casvg/mera.svg";
+export { default as clockIcon } from "./csvg/lock.svg";
+export { default as locationIcon } from "./locasvg/tion.svg";
+export { default as mailIcon } from "./svg/mail.svg";
+export { default as notFound } from "./not-fsvg/ound.svg";
+export { default as sendIcon } from "./psvg/lane.svg";
+export { default as searchIcon } from "./sesvg/arch.svg";
+
+export { default as chatIcon } from "./svg/chat.svg";
+export { default as closeIcon } from "./csvg/lose.svg";
+export { default as fileIcon } from "./svg/file.svg";
+export { default as hamburger } from "./hambusvg/rger.svg";
+export { default as optionsIcon } from "./optsvg/ions.svg";
+export { default as logoutIcon } from "./losvg/gout.svg";
+export { default as homeIcon } from "./svg/home.svg";
+
+export { default as dp } from "./images/dp.jpg";
