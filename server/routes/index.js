3c459f18bb88aba2fd473c34ae07a6eebdd19b32
Undefined-deve1oper <kononovkirill05@gmail.com>
2023-03-11 14:23:43 +0600

    added socket.io

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,10 +2,15 @@ const express = require("express");
 const router = express.Router({ mergeParams: true });
 const userRouter = require("./user.routes");
 const authRouter = require("./auth.routes");
-const postsRouter = require("./post.routes");
+const postRouter = require("./post.routes");
+const chatRouter = require("./routes/chat");
+const messageRouter = require("./routes/message");
+const authorizationMiddleware = require("../middleware/authorization");
 
 router.use("/users", userRouter);
 router.use("/auth", authRouter);
-router.use("/posts", postsRouter);
+router.use("/posts", postRouter);
+router.use("/chats", authorizationMiddleware, chatRouter);
+router.use("/messages", authorizationMiddleware, messageRouter);
 
 module.exports = router;
